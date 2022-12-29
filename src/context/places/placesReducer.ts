277c6319bb58f0008177664f942275769338a680
b0pez <blopez4434@gmail.com>
2022-12-29 01:30:36 -0300

    creando reducer

--- src/context/places/placesReducer.ts
+++ src/context/places/placesReducer.ts
@@ -1,3 +1,17 @@
-export const placesReducer = (state: any, action: any) => {
+import { PlacesState } from "./PlacesProvider";
+
+type PlacesAction = {type: 'setUserLocation', payload: [number, number]};
+
+export const placesReducer = (state: PlacesState, action: PlacesAction): PlacesState => {
+    switch (action.type) {
+        case 'setUserLocation':
+            return {
+                ...state,
+                isLoading: false,
+                userLocation: action.payload
+            }
     
+        default:
+            return state;
+    }
 }
