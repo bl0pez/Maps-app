0118b54f6aabfc200e7bbb1e265535908d736bc1
b0pez <blopez4434@gmail.com>
2023-01-01 01:28:38 -0300

    creando contexto y reducer map

--- src/MapsApp.tsx
+++ src/MapsApp.tsx
@@ -2,11 +2,14 @@ import { PlacesProvider } from './context';
 import { Home } from './screens';
 
 import './index.css';
+import { MapProvider } from './context/map/MapProvider';
 
 export const MapsApp = () => {
   return (
     <PlacesProvider>
+      <MapProvider>
         <Home />
+      </MapProvider>
     </PlacesProvider>
   )
 }
--- src/context/index.ts
+++ src/context/index.ts
@@ -1,3 +1,5 @@
+import { MapContext } from "./map/MapContext";
+import { MapProvider } from "./map/MapProvider";
+
 export { PlacesContext } from "./places/PlacesContext";
 export { PlacesProvider } from "./places/PlacesProvider";
-
