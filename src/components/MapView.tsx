2ce0dda8cdde69132257ea178050a822d424d062
b0pez <blopez4434@gmail.com>
2023-01-01 01:15:43 -0300

    Mostrando mapa

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,9 +1,24 @@
-import { useContext } from 'react';
+import { useContext, useLayoutEffect, useRef } from 'react';
 import { PlacesContext } from '../context';
+import { Map } from 'mapbox-gl';
 import { Loading } from './';
 
 export const MapView = () => {
     const { isLoading, userLocation } = useContext(PlacesContext);
+    console.log({userLocation});
+    
+    const mapDiv = useRef<HTMLDivElement>(null);
+
+    useLayoutEffect(() => {
+        if( !isLoading){
+            const map = new Map({
+                container: mapDiv.current!,
+                style: 'mapbox://styles/mapbox/streets-v12', // style URL
+                center: userLocation, // starting position [lng, lat]
+                zoom: 9, // starting zoom
+                });
+        }
+    }, [ isLoading, userLocation ])
 
     if (isLoading) {
         return (<Loading />)
@@ -11,7 +26,14 @@ export const MapView = () => {
 
 
     return (
-        <div>
+        <div ref={mapDiv}
+            style={{
+                height: '100%',
+                width: '100%',
+                position: 'fixed',
+                top: 0,
+            }}
+        >
             {userLocation?.join(', ')}
         </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnJ5YW5sb3BlenMiLCJhIjoiY2t0OHNuamhnMTR5ODJ3cGxlcnF5OWg1OCJ9.ZYmJvqeqJ40HbkMbnUW_cw';
+
 if( !navigator.geolocation ) {
   alert('Tu navegador no soporta geolocalización');
   throw new Error('Tu navegador no soporta geolocalización');
